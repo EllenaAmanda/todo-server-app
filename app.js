b71f43f8e5938670fa8723b09ceacbc2c46cee21
EllenaAmanda <ellenaamanda846@gmail.com>
2024-10-18 02:26:46 +0700

    berhasil connect ke mongodb

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const allRoutes = require("./routes")
+const db = require("./db")
 
 app.use(allRoutes)
+
+db.then(() => {
+    console.log("berhasil connect ke db")
+})
+.catch(() => {
+    console.log("gagal connect ke db")
+})
 
 app.listen(PORT, ()=> {
     console.log("Server is running in PORT " + PORT)
