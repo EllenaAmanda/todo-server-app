6bdb3a4ac56fe74ea15e940df631e7c4afa193fe
EllenaAmanda <ellenaamanda846@gmail.com>
2024-10-18 23:22:20 +0700

    added another status code

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -2,63 +2,118 @@ const Todo = require("../models/Todo");
 
 module.exports = {
     getAllTodos: async(req,res) => {
-        const data = await Todo.find({})
+        try {
+            const data = await Todo.find({});
 
-        res.status(200).json({
-            message: "Berhasil mengambil semua Todos",
-            data
-        })
+            res.status(200).json({
+                message: "Berhasil mengambil semua Todos",
+                data
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal mengambil todos",
+                error: error.message
+            });
+        }
     },
     getTodoById: async(req,res) => {
-        const {id} = req.params
+        try {
+            const { id } = req.params;
+            const data = await Todo.findById(id).exec();
 
-        const data = await Todo.findById(id).exec()
+            if (!data) {
+                return res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                });
+            }
 
-        res.status(200).json({
-            message: "Berhasil mengambil todo by id",
-            data
-        })
+            res.status(200).json({
+                message: "Berhasil mengambil todo by id",
+                data
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal mengambil todo",
+                error: error.message
+            });
+        }
     },
-    addTodo: (req,res) => {
+    addTodo: async(req,res) => {
+        try {
+            const data = req.body; // input data
 
-        const data = req.body; // inputan baru
+            const newTodo = new Todo(data); // create new Todo instance
+            await newTodo.save(); // save to database
 
-        const newTodo = new Todo(data) // panggil model dan masukkan data baru
-
-        newTodo.save()
-
-        res.status(201).json({
-            message:"data berhasil dibuat",
-        })
+            res.status(201).json({
+                message: "Todo berhasil dibuat",
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal membuat todo",
+                error: error.message
+            });
+        }
     },
 
     updateTodoById: async (req,res) => {
-        const {id} = req.params
-        const data = req.body;
-        // console.log(id)
-        // console.log(data)
+        try {
+            const { id } = req.params;
+            const data = req.body;
 
-        await Todo.findByIdAndUpdate(id,data)
+            const updatedTodo = await Todo.findByIdAndUpdate(id, data, { new: true });
 
-        res.status(200).json({
-            message:"todo berhasil di update"
-        })
+            if (!updatedTodo) {
+                return res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                });
+            }
+
+            res.status(200).json({
+                message: "Todo berhasil di update",
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal mengupdate todo",
+                error: error.message
+            });
+        }
     },
     deleteAllTodos: async (req,res) => {
-        await Todo.deleteMany({})
+        try {
+            await Todo.deleteMany({});
 
-        res.status(200).json({
-            message: "todo berhasil di hapus semua"
-        })
+            res.status(200).json({
+                message: "Semua todos berhasil dihapus"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal menghapus semua todos",
+                error: error.message
+            });
+        }
     },
     deleteTodoById: async (req,res) => {
-        const {id} = req.params
+        try {
+            const { id } = req.params;
 
-        await Todo.findByIdAndDelete(id)
+            const deletedTodo = await Todo.findByIdAndDelete(id);
 
-        res.status(200).json({
-            message:"todo berhasil di delete berdasarkan id"
-        })
+            if (!deletedTodo) {
+                return res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                });
+            }
+
+            res.status(200).json({
+                message: "Todo berhasil dihapus"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal menghapus todo",
+                error: error.message
+            });
+        }
     },
 
 }
